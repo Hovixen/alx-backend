625107299bf5b8dbc790abce238b4e7a002e1b0e
Hovixen <orevaerijo@gmail.com>
2024-05-28 15:40:23 +0000

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Basic Flask App """
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """ config class for babel languages """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ determines the best language match """
+    return request.accept_languages.best_match(
+            app.config['LANGUAGES']
+    )
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """ index function """
+    title = "Welcome to Holberton"
+    return render_template("0-index.html", title=title)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
