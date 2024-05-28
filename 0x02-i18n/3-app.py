7d1d9eb6d8394fb6c228141ebba2d5d1a284289c
Hovixen <orevaerijo@gmail.com>
2024-05-28 17:50:58 +0000

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,7 +22,9 @@ def get_locale() -> str:
     """ determines the best language match """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
