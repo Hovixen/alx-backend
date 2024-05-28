26fbee2a81781915922905aea20be3a041b5697b
Hovixen <orevaerijo@gmail.com>
2024-05-28 15:23:03 +0000

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,7 +6,7 @@ from flask_babel import Babel
 app = Flask(__name__)
 
 
-class Config(object):
+class Config:
     """ config class for babel languages """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
